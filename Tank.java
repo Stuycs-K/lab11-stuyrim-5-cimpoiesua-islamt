eb211b7dae7c3ea1eac7508d149e120438f702b2
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2025-01-10 11:28:48 -0500

    wrote specialAttack for the tank class

--- Tank.java
+++ Tank.java
@@ -18,10 +18,25 @@ public class Tank extends Adventurer{
     return foodMax;
   }
   public String attack(Adventurer other){
+    if(Math.random >= 0.1){
     int damage = (int)(Math.random()*3)+2;
     other.applyDamage(damage);
     restoreSpecial(2);
     return this + " punched "+ other + " and dealt "+ damage +
     " points of damage.";
+    }
+    else {
+      return this + " attack on " + other + " failed! Better luck next time.";
+    }
+  }
+  public String specialAttack(Adventurer other){
+  if(getSpecial() >= 15){
+    setSpecial(getSpecial()-15);
+    int damage = (int)(Math.random()*2)+12;
+    other.applyDamage(damage);
+    return this + " body slammed on "+other+
++ " dealing "+ damage +" points of damage.";
+    }else{
+      return "Not enough food to body slam. Get to eating!"+attack(other);
   }
 }
