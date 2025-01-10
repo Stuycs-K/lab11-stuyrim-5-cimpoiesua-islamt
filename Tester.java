bf31e73af9e1df55c101e852906086e42621d555
Tasnim Islam <tasnim.nyc@gmail.com>
2025-01-10 11:25:36 -0500

    colored and tested

--- Tester.java
+++ Tester.java
@@ -1,5 +1,9 @@
 public class Tester {
   public static void main(String[] args) {
-    
+    // Testing coloring
+    System.out.println(Game.colorByPercent(3,4));
+    System.out.println(Game.colorByPercent(4,4));
+    System.out.println(Game.colorByPercent(2,4));
+    System.out.println(Game.colorByPercent(1,5));
   }
 }
