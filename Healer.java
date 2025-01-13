7d8903095f4dc5fdc573fa08d61911d51501c610
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2025-01-13 11:19:14 -0500

    made Healer class and its constructor and an accessor method

--- Healer.java
+++ Healer.java
@@ -1,3 +1,14 @@
 public class Healer extends Adventurer{
-  
+  int spirits, spiritsmax;
+  public Healer(String name, int hp){
+    super(name,hp);
+    foodMax = 15;
+    food = foodMax/2;
+  }
+  public String getSpecialName(){
+    return "spirits";
+  }
+  public int getSpecial(){
+    return spirits;
+  }
 }
