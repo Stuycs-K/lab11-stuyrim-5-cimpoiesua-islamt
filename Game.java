e12f61f138f5b172f32cf5902136ff2dc9085d77
Tasnim Islam <tasnim.nyc@gmail.com>
2025-01-13 11:29:15 -0500

    Fixing up the background creation

--- Game.java
+++ Game.java
@@ -13,19 +13,21 @@ public class Game{
   //Do not write over the blank areas where text will appear or parties will appear.
   public static void drawBackground(){
     /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+    Text.hideCursor();
     Text.clear();
-    Text.go(1,1);
-    for(int i =0; i < 80; i++) {
+    for(int i =1; i < 81; i++) {
+    Text.go(1,i);
     System.out.print("-");
   }
     for (int i =2; i < 30; i++) {
-      Text.go(1,i);
+      Text.go(i,1);
       System.out.print("|");
-      Text.go(80,i);
+      Text.go(i,80);
       System.out.print("|");
       }
    Text.go(1,30);
    for(int i =0; i < 80; i++) {
+     Text.go(30,i);
    System.out.print("-");
       }
     /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
