fd88636dd7ee24931abdd6fba0c1151ff948765f
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2025-01-16 11:40:09 -0500

    added accessor methods for Boss class

--- Boss.java
+++ Boss.java
@@ -1,3 +1,28 @@
-public class Tank extends Boss{
+public class Boss extends Adventurer{
+  int bats, batMax;
+  boolean isRegenerating, isParalyzed;
+  int turnregens, turnparalyze;
+
+  public Boss(String name, int hp) {
+    super(name, (int) 2 * hp);
+    batMax = 22;
+    bats = batMax / 2 + 1;
+  }
+
+  public String getSpecialName() {
+    return "bats";
+  }
+
+  public int getSpecial() {
+    return bats;
+  }
+
+  public void setSpecial(int n) {
+    bats = n;
+  }
+
+  public int getSpecialMax() {
+    return batMax;
+  }
 
 }
